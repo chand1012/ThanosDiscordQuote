a54c444b634cc4ccd7859c1197c90875b436311d
Chandler Lofland <chandlerl14@outlook.com>
2019-03-30 23:45:51 -0400

    changed lots of things

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 #Random Thanos
+https://discordapp.com/api/oauth2/authorize?client_id=561734227448823838&permissions=6144&scope=bot
--- bot.py
+++ bot.py
@@ -1,2 +1,47 @@
 import discord
 import requests
+import json
+
+def get_keys(filename="keys.json"):
+    parsed = None
+    with open(filename) as keyfile:
+        raw = keyfile.read()
+        parsed = json.loads(raw)
+    return parsed
+
+def get_quote():
+    data = requests.get("https://thanosapi.herokuapp.com/random/")
+    parsed_data = json.loads(data.text)
+    return parsed_data['quote']
+
+keys = get_keys()
+token = keys['discord_token']
+client = discord.Client()
+@client.event
+async def on_message(message):
+    recv = message.content
+    channel = message.channel
+    if message.author == client.user:
+        pass
+    if recv.startswith('!thanos'):
+        msg = get_quote()
+        state = recv[8:] is ''
+        await client.send_message(channel, content=msg, tts=state)
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+while True:
+    try:
+        client.run(token)
+    except Exception as e:
+        if "Event loop" in str(e):
+            print("\nStopping bot....")
+            break
+        else:
+            print(e)
+            continue
